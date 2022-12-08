ac401eb13a1747ad16f24a2c266eb3220e6d89f0
Tamsters24 <102896617+Tamsters24@users.noreply.github.com>
2022-12-07 18:32:39 -0800

    Mock Lab 2 Starter

---
+++ MockLab2Starter.py
@@ -0,0 +1,24 @@
+# Requirements:
+# * You must have variables named:
+#       price, change, quarters, dimes, nickels, and pennies.
+# * You may include any additional variables you choose.
+# * In your finished code, put all print statements at the bottom
+#   (as indicated by the comment).
+# * You may use print statements elsewhere in your code while you are developing
+#   and testing, but you should comment out those print statements before submission.
+
+
+# once you have a working solution, you should change this line to test other
+# starting values, e.g., 68 cents, 69 cents, etc. to ensure that your calculations
+# work for each value.
+price = 67
+
+# do all of your computations using the provided variables here:
+#change =
+#quarters =
+#dimes =
+#nickels =
+#pennies =
+
+# put all print statements in this section:
+print('The price is', price,'cents.' )
